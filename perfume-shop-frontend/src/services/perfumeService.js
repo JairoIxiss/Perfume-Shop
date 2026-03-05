4d22b1803c142f242ef71b4aa47480ba73442215
JairoIxiss <jairoixis7@gmail.com>
2026-03-05 14:15:16 -0600

    implementacion del servicio de Perfume

---
+++ perfume-shop-frontend/src/services/perfumeService.js
@@ -0,0 +1,33 @@
+import api from './api';
+
+export const perfumeService = {
+    getAll: async () => {
+        const response = await api.get('/perfume/traer');
+        return response.data;
+    },
+
+    getById: async (id) => {
+        const response = await api.get('/perfume/traer/${id}');
+        return response.data;
+    },
+
+    getByGenero: async () => {
+        const response = await api.get('/perfume/traer/por-genero/${genero}');
+        return response.data;
+    },
+
+    create: async (perfume) => {
+        const response = await api.post('/perfume/crear', perfume);
+        return response.data;
+    },
+
+    update: async (id, perfume) => {
+        const reponse = await api.put('/perfume/editar/${id}', perfume);
+        return reponse.data;
+    },
+
+    delete: async (id) => {
+        const response = await api.delete('/perfume/eliminar/${id}');
+        return response.data;
+    },
+};
